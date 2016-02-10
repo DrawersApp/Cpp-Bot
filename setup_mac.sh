e9127e33422de60ad08474702ad2adf678651e03
Nishant-Pathak <nishantpathak.cse@gmail.com>
2016-02-10 21:00:21 +0530

    minor fix

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -x -e
-cd lib
-git submodule init
-git submodule update
-cd restclient-cpp
-./autogen.sh
-./configure
-sudo make install
 direc=`pwd`
 cd /tmp
 curl -O http://camaya.net/download/gloox-1.0.14.tar.bz2
@@ -14,7 +7,6 @@ tar -xzvf gloox-1.0.14.tar.bz2
 cd gloox-1.0.14
 ./configure
 make
-make install
+sudo make install
 cd $direc
-cd ../..
 make all
